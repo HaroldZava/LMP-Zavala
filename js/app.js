eb69636f8b06b2d58aeed281f13fbe21e84f85e3
HaroldZavala <haroldzava@gmail.com>
2022-09-13 20:05:21 -0500

    Implementando vista tablet

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@
 
 
     window.addEventListener('resize', ()=>{
-        if(window.innerWidth > 480){
+        if(window.innerWidth > 768){
             deleteStyleHeight();
             if(list.classList.contains('menu__links--show'))
                 list.classList.remove('menu__links--show');
@@ -46,7 +46,7 @@
         }
     });
 
-    if(window.innerWidth <= 480){
+    if(window.innerWidth <= 768){
         addClick();
     }
 
